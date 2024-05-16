fd9484b9fd981575ed82ac4acd2d94d6df1f08a5
qiuxan <qiuxan@gmail.com>
2024-05-17 09:36:33 +1000

    update the route with /api

--- healthcheck.client/src/proxy.conf.js
+++ healthcheck.client/src/proxy.conf.js
@@ -6,7 +6,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/weatherforecast",
+      "/api",
     ],
     target, target: "https://localhost:40443",
     secure: false
