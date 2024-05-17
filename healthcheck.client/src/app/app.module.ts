00d8f9e926f46c86f0ec0648c44efa3f7156b349
qiuxan <qiuxan@gmail.com>
2024-05-18 07:04:09 +1000

    build the basic application with navbar and http client

--- healthcheck.client/src/app/app.module.ts
+++ healthcheck.client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavMenuComponent,
+    FetchDataComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
