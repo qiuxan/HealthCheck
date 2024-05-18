3ffec6d90e5af8a3803e89073f4f54d066211d43
qiuxan <qiuxan@gmail.com>
2024-05-19 09:43:55 +1000

    Healthy Check setu up backend

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -1,4 +1,3 @@
-
 namespace HealthCheck.Server
 {
     public class Program
@@ -8,7 +7,13 @@ namespace HealthCheck.Server
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddHealthChecks();
+            builder.Services.AddHealthChecks()
+                .AddCheck("ICMP_01",
+                    new ICMPHealthCheck("www.ryadel.com", 100))
+                .AddCheck("ICMP_02",
+                    new ICMPHealthCheck("www.google.com", 100))
+                .AddCheck("ICMP_03",
+                    new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -30,7 +35,8 @@ namespace HealthCheck.Server
 
             app.UseAuthorization();
 
-            app.UseHealthChecks(new PathString("/api/health"));
+            app.UseHealthChecks(new PathString("/api/health"),
+                new CustomHealthCheckOptions());
             app.MapControllers();
 
             app.MapFallbackToFile("/index.html");
