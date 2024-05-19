12dbcbd9ad50e1d35c73132450337aa0bc501202
qiuxan <qiuxan@gmail.com>
2024-05-19 20:45:11 +1000

    Use scss for the project to replace css cli: > ng add schematics-scss-migrate

--- healthcheck.client/src/app/health-check/health-check.component.ts
+++ healthcheck.client/src/app/health-check/health-check.component.ts
@@ -4,7 +4,7 @@ import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-health-check',
   templateUrl: './health-check.component.html',
-  styleUrls: ['./health-check.component.css']
+  styleUrls: ['./health-check.component.scss']
 })
 export class HealthCheckComponent implements OnInit {
   public result?: Result;
